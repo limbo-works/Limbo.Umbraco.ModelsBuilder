75c1ba197bb27e1f30d25826f87ee18d00aa71b0
Anders Bjerner <abjerner@skybrud.dk>
2022-01-27 13:51:32 +0100

    Added API controller for building models

---
+++ src/Limbo.Umbraco.ModelsBuilder/Controllers/ModelsBuilderController.cs
@@ -0,0 +1,55 @@
+﻿using Limbo.Umbraco.ModelsBuilder.Models;
+using Limbo.Umbraco.ModelsBuilder.Services;
+using Limbo.Umbraco.ModelsBuilder.Settings;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using Umbraco.Cms.Web.BackOffice.Controllers;
+using Umbraco.Cms.Web.Common.Attributes;
+
+#pragma warning disable 1591
+
+namespace Limbo.Umbraco.ModelsBuilder.Controllers {
+    
+    [PluginController("Limbo")]
+    public class ModelsBuilderController : UmbracoAuthorizedApiController {
+        
+        private readonly ILogger<ModelsBuilderController> _logger;
+        private readonly ModelsGenerator _modelsGenerator;
+        private readonly ModelsSourceGenerator _sourceGenerator;
+
+        public ModelsBuilderController(ILogger<ModelsBuilderController> logger, ModelsGenerator modelsGenerator, ModelsSourceGenerator sourceGenerator) {
+            _logger = logger;
+            _modelsGenerator = modelsGenerator;
+            _sourceGenerator = sourceGenerator;
+        }
+
+        [HttpGet]
+        public object GenerateModels() {
+
+            try {
+
+                // Get the default settings
+                ModelsGeneratorSettings settings = _modelsGenerator.GetDefaultSettings();
+
+                // Generate definitions for all the models
+                TypeModelList models = _modelsGenerator.GetModels();
+
+                // Generate the source code and save the models to disk
+                _sourceGenerator.SaveModels(models, settings);
+
+                return new { success = true };
+
+            } catch (Exception ex) {
+
+                _logger.LogError(ex, "Failed building models.");
+
+                return new { success = false };
+
+            }
+
+        }
+
+    }
+
+}
