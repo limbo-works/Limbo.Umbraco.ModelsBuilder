e5caa2be83037a6393c4fe853521639b28762aad
Anders Bjerner <abjerner@skybrud.dk>
2022-02-23 20:55:29 +0100

    Introduced new API method for getting the Models Builder status

--- src/Limbo.Umbraco.ModelsBuilder/Controllers/ModelsBuilderController.cs
+++ src/Limbo.Umbraco.ModelsBuilder/Controllers/ModelsBuilderController.cs
@@ -3,7 +3,9 @@ using Limbo.Umbraco.ModelsBuilder.Services;
 using Limbo.Umbraco.ModelsBuilder.Settings;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using System;
+using Umbraco.Cms.Infrastructure.ModelsBuilder;
 using Umbraco.Cms.Web.BackOffice.Controllers;
 using Umbraco.Cms.Web.Common.Attributes;
 
@@ -15,13 +17,39 @@ namespace Limbo.Umbraco.ModelsBuilder.Controllers {
     public class ModelsBuilderController : UmbracoAuthorizedApiController {
         
         private readonly ILogger<ModelsBuilderController> _logger;
+        private readonly OutOfDateModelsStatus _outOfDateModelsStatus;
+        private readonly IOptions<LimboModelsBuilderSettings> _modelsBuilderSettings;
         private readonly ModelsGenerator _modelsGenerator;
         private readonly ModelsSourceGenerator _sourceGenerator;
 
-        public ModelsBuilderController(ILogger<ModelsBuilderController> logger, ModelsGenerator modelsGenerator, ModelsSourceGenerator sourceGenerator) {
+        public ModelsBuilderController(ILogger<ModelsBuilderController> logger, OutOfDateModelsStatus outOfDateModelsStatus,
+            IOptions<LimboModelsBuilderSettings> modelsBuilderSettings, ModelsGenerator modelsGenerator, ModelsSourceGenerator sourceGenerator) {
             _logger = logger;
+            _outOfDateModelsStatus = outOfDateModelsStatus;
+            _modelsBuilderSettings = modelsBuilderSettings;
             _modelsGenerator = modelsGenerator;
             _sourceGenerator = sourceGenerator;
+        }
+        
+        [HttpGet]
+        public object GetStatus() {
+
+            try {
+
+                return new {
+                    version = ModelsBuilderPackage.SemVersion.ToString(),
+                    mode = _modelsBuilderSettings.Value.ModelsMode,
+                    outOfDateModels = _outOfDateModelsStatus.IsOutOfDate
+                };
+
+            } catch (Exception ex) {
+
+                _logger.LogError(ex, "Failed building models.");
+
+                return new { success = false };
+
+            }
+
         }
 
         [HttpGet]
