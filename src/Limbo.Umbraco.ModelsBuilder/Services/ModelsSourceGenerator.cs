27286c8a10410b6d0122f2b7420473ea64231d07
Anders Bjerner <abjerner@skybrud.dk>
2022-08-24 14:46:59 +0200

    FFS 🤦‍♂️
    Fix for #12

--- src/Limbo.Umbraco.ModelsBuilder/Services/ModelsSourceGenerator.cs
+++ src/Limbo.Umbraco.ModelsBuilder/Services/ModelsSourceGenerator.cs
@@ -275,7 +275,7 @@ namespace Limbo.Umbraco.ModelsBuilder.Services {
 
             if (type is ModelType modelType) {
                 if (models.TryGetModel(modelType.ContentTypeAlias, out var actualTypeName)) {
-                    return actualTypeName.Namespace == model.Namespace ? string.Empty : actualTypeName.Namespace + "." + actualTypeName.ClrName;
+                    return actualTypeName.Namespace == model.Namespace ? actualTypeName.ClrName : actualTypeName.Namespace + "." + actualTypeName.ClrName;
                 }
                 throw new InvalidOperationException($"Don't know how to map ModelType with content type alias \"{modelType.ContentTypeAlias}\".");
             }
