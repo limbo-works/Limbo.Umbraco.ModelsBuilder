20e156155ae96282115ff437a6e2918b78c42938
Anders Bjerner <abjerner@skybrud.dk>
2023-09-02 16:00:55 +0200

    Updated package manifest filter to set "PackageId" property in Umbraco 12

--- src/Limbo.Umbraco.ModelsBuilder/Manifests/ModelsBuilderManifest.cs
+++ src/Limbo.Umbraco.ModelsBuilder/Manifests/ModelsBuilderManifest.cs
@@ -1,5 +1,5 @@
-﻿using Skybrud.Essentials.Strings.Extensions;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Reflection;
 using Umbraco.Cms.Core.Manifest;
 
 #pragma warning disable CS1591
@@ -9,18 +9,30 @@ namespace Limbo.Umbraco.ModelsBuilder.Manifests {
     public class ModelsBuilderManifest : IManifestFilter {
 
         public void Filter(List<PackageManifest> manifests) {
-            manifests.Add(new PackageManifest {
+
+            // Initialize a new manifest filter for this package
+            PackageManifest manifest = new() {
                 AllowPackageTelemetry = true,
                 PackageName = ModelsBuilderPackage.Name,
                 Version = ModelsBuilderPackage.InformationalVersion,
                 BundleOptions = BundleOptions.Independent,
-                Scripts = new[] {
-                    $"/App_Plugins/{ModelsBuilderPackage.Alias}/Scripts/Controllers/Dashboard.js"
-                },
-                Stylesheets = new[] {
-                    $"/App_Plugins/{ModelsBuilderPackage.Alias}/Styles/Default.css"
-                }
-            });
+                Scripts = new[] { $"/App_Plugins/{ModelsBuilderPackage.Alias}/Scripts/Controllers/Dashboard.js" },
+                Stylesheets = new[] { $"/App_Plugins/{ModelsBuilderPackage.Alias}/Styles/Default.css" }
+            };
+
+            // The "PackageId" property isn't available prior to Umbraco 12, and since the package is build against
+            // Umbraco 10, we need to use reflection for setting the property value for Umbraco 12+. Ideally this
+            // shouldn't fail, but we might at least add a try/catch to be sure
+            try {
+                PropertyInfo? property = manifest.GetType().GetProperty("PackageId");
+                property?.SetValue(manifest, ModelsBuilderPackage.Alias);
+            } catch {
+                // We don't really care about the exception
+            }
+
+            // Append the manifest
+            manifests.Add(manifest);
+
         }
 
     }
